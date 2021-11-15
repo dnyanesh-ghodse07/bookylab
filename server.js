19347d7e38f32c3a5155d6bcf94ac8ad501e1c47
dghodse <ghodsednyaneshwar@gmail.com>
2021-11-15 15:26:33 +0530

    port changed

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ db.once('open', () => console.log("Connected DB"))
 
 app.use('/', indexRouter);
 
-app.listen(process.env.PORT || 3000,() => {
+app.listen(process.env.port || 3000,() => {
     console.log("Server starting on port 3000");
 })
